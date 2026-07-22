bbe7b2cb76a9c43a690653725efd54d2b88c2ced
Thedoom19 <69268200+Thedoom19@users.noreply.github.com>
2026-07-22 16:41:47 +0200

    made pre-4.6 changes

--- addons/music_player/plugin.gd
+++ addons/music_player/plugin.gd
@@ -1,24 +1,17 @@
 @tool
 extends EditorPlugin
 
-var dock : EditorDock
+var dock : Control
 
 
 func _enter_tree() -> void:
-	var dockScene = preload("res://addons/music_player/music_player.tscn").instantiate()
+	dock = preload("res://addons/music_player/music_player.tscn").instantiate()
 	
-	dock = EditorDock.new()
-	dock.add_child(dockScene)
-	
-	dock.default_slot = EditorDock.DOCK_SLOT_LEFT_BR
-	
-	dock.available_layouts = EditorDock.DOCK_LAYOUT_VERTICAL | EditorDock.DOCK_LAYOUT_FLOATING
-	
-	add_dock(dock)
+	add_control_to_dock(DOCK_SLOT_LEFT_BR, dock)
 
 
 
 func _exit_tree() -> void:
-	remove_dock(dock)
+	remove_control_from_docks(dock)
 	
 	dock.queue_free()
